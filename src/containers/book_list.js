38325eed7d14ded5af2f0275abb02a4540bee80d
raquelthiele <raquelthiele@hotmail.com>
2018-08-10 18:02:26 -0300

    Reset

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 
   renderList(){
     return this.props.books.map((book) => {
       return (
-        <li key="books.title" className="list-group-item">{books.title}</li>
+        <li key={book.title} className="list-group-item">{book.title}</li>
       );
     });
   }
@@ -18,3 +19,11 @@ export default class BookList extends Component {
     );
   }
 };
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
